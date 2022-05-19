5163fd617e1760d090929ba7a15a9292d63ff16d
SebAti95 <102329383+SebAti95@users.noreply.github.com>
2022-05-19 17:47:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//Ejercicio 1
+
+var a = 5;
+var b = 10;
+var c = "I ana";
+
+a = a +1;
+b = b+5
+c = c+"String"
+
+//Delcaracion de variables
+
+var StudlyCaseVar;
+var ProPercamelcase
+var TitleCaseOver;
+
+//Asignacion de valores:
+
+StudlyCapVar = 10;
+ProperCamelCase = "A String";
+TitleCaseOver = 2000;
+
+//Sustituir con let o const
+
+var gLB = "gabrielabeatriz";
+var fact = "is cool";
+const fact = "is awesome";
+console.log (gLB, fact);
